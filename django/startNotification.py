4a554dc202b8048e4ab1adc5d2ddd664e1e96f48
umi <bo314@naver.com>
2020-03-09 19:08:30 +0900

    Removed exposed slack webhook

--- django/startNotification.py
+++ django/startNotification.py
@@ -1,3 +1,12 @@
 import requests
-r = requests.post("https://hooks.slack.com/services/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", json={"text":"Created a new ipmat-server container"})
+import dotenv
+import os
+from dotenv import load_dotenv
+IPMAT_MODE=os.getenv("IPMAT_MODE") if os.getenv("IPMAT_MODE") else "development"
+if IPMAT_MODE=="development":
+    load_dotenv()
+elif IPMAT_MODE=="production":
+    load_dotenv(".production.env")
+
+r = requests.post(os.getenv("IPMAT_SLACK_URL"), json={"text":"Created a new ipmat-server container"})
 print(r.text)
